fea91dce134a55f1dd0348721a428c806d7900cb
kaganaslan <vatikaanaslan@gmail.com>
2023-10-12 00:37:16 +0300

    Interfaces added
    Migration Abstract IRepositories added

---
+++ MvcSozluk/DataAccessLayer/Abstract/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IRepository<T>
+    {
+        //Burada bir sürü tablo olabileceği için bunu parametreli hale getirerek genel bi repository oluşturdum ve bunları parametreli şekilde çağırdım.
+        List<T> GetAll();
+        void Insert(T p);
+        void Delete(T p);
+        void update(T p);
+    }
+}

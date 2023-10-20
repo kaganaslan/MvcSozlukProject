bca8501914f5d0b1f6768a8ab00ed083e021a2ba
kaganaslan <vatikaanaslan@gmail.com>
2023-10-20 22:59:43 +0300

    added and updated

---
+++ MvcSozluk/MvcSozluk/Controllers/MessageController.cs
@@ -0,0 +1,37 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcSozluk.Controllers
+{
+    public class MessageController : Controller
+    {
+        // GET: Message
+        MessageManager mm = new MessageManager(new EfMessageDal());
+        public ActionResult Inbox()
+        {
+            var messageList = mm.GetListInbox();
+            return View(messageList);
+        }
+        public ActionResult Sentbox()
+        {
+            var messagelist = mm.GetSentInbox();
+            return View(messagelist);
+        }
+        [HttpGet]
+        public ActionResult NewMessage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NewMessage(Message p)
+        {
+            return View();
+        }
+    }
+}

7de8347bdecd66b71d510e0002ae36ec8d97367c
kaganaslan <vatikaanaslan@gmail.com>
2023-10-26 00:17:31 +0300

    Updated
    Message Parts Added

--- MvcSozluk/MvcSozluk/Controllers/MessageController.cs
+++ MvcSozluk/MvcSozluk/Controllers/MessageController.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,10 +15,22 @@ namespace MvcSozluk.Controllers
     {
         // GET: Message
         MessageManager mm = new MessageManager(new EfMessageDal());
+        MessageValidator messageValidator = new MessageValidator();
         public ActionResult Inbox()
         {
             var messageList = mm.GetListInbox();
             return View(messageList);
+        }
+        public ActionResult GetInBoxMessageDetails(int id)
+        {
+            var values = mm.GetByID(id);
+            return View(values);
+        }
+
+        public ActionResult GetSendBoxMessageDetails(int id)
+        {
+            var values = mm.GetByID(id);
+            return View(values);
         }
         public ActionResult Sentbox()
         {
@@ -31,6 +45,20 @@ namespace MvcSozluk.Controllers
         [HttpPost]
         public ActionResult NewMessage(Message p)
         {
+            ValidationResult results = messageValidator.Validate(p);
+            if (results.IsValid)
+            {
+                p.MessageDate=DateTime.Parse(DateTime.Now.ToShortDateString());
+                mm.MessageAdd(p);
+                return RedirectToAction("SentBox");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
             return View();
         }
     }
